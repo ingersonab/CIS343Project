75bdbcc04e38c39b222cef9f68782d097bd10b0c
Eyesackcursey <116756881+Eyesackcursey@users.noreply.github.com>
2023-04-20 15:17:21 -0400

    Box class init Item class init Items packing class init

---
+++ src/Problem2/Box.java
@@ -0,0 +1,20 @@
+package Problem2;
+
+import java.util.ArrayList;
+
+public class Box {
+
+    final double capacity = 1.0;
+    ArrayList<Item> items = new ArrayList<>();
+
+    public Box() {
+
+    }
+
+    public void add(Item x) {
+
+
+    }
+
+
+}
